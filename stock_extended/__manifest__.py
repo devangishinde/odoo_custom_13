abc62128550a703eb43cd3a8539844cb3ced7e58
Devangi Shinde <devangi.shinde66@gmail.com>
2022-09-06 15:38:57 +0530

    [ADD][stock_extended] add stock_extended module

---
+++ stock_extended/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Stock Extended',
+	'summary': 'Stock Extended',
+	'sequence': 3,
+	'version': '1.6',
+	'depends': [
+		'base',
+		'web',
+		'stock'
+	],
+	'data': [
+  		'data/res_company_data.xml',
+    	'data/stock_picking_type_data.xml',
+	],
+	'installable': True,
+	'application': True
+}
